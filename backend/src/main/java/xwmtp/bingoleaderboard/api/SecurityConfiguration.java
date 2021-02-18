f3929cb41200ad3ab2f9f9da5b06052d72e39fd9
xwmtp <xwillmtp@gmail.com>
2021-02-18 10:16:55 +0100

    allow iframe

--- backend/src/main/java/xwmtp/bingoleaderboard/api/SecurityConfiguration.java
+++ backend/src/main/java/xwmtp/bingoleaderboard/api/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,6 +20,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .anyRequest().permitAll();
+
+        http.headers()
+                .frameOptions().disable()
+                .addHeaderWriter(
+                        new StaticHeadersWriter("X-FRAME-OPTIONS", "ALLOW-FROM xwmtp.github.io")
+                );
     }
 
     @Bean
