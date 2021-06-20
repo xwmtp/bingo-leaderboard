8037dc868bd547ee2221098e0996d23ab72c0822
xwmtp <xwillmtp@gmail.com>
2021-06-20 13:59:14 +0200

    update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+git pull
 ./gradlew clean
 npm --prefix frontend install
 rm -rf frontend/build
 npm run --prefix frontend build
 cp -r frontend/build backend/src/main/resources/static
-git pull
 ./gradlew build
-docker-compose build --pull
-docker-compose up -d
+sudo docker-compose build --pull
+sudo docker-compose up -d
